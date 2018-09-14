0d58bec220ad806eaef802a5c08d552191076a86
yokoyuta <yutaka@yokogainoMacBook-puro.local>
2018-09-14 21:36:46 +0900

    Userモデルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages/index'
+  devise_for :users
+  root 'pages#index'
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
